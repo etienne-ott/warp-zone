1813ce010454041134347f36614bec8ae6f02c63
Etienne Ott <eti.ott@gmail.com>
2015-02-20 15:54:13 +0100

    Applied code and documentation standards to Section and Entry classes.

--- Entry.php
+++ Entry.php
@@ -1,6 +1,12 @@
 <?php
-
+/**
+ * Represents an entry containing data for an URL (e.g. name, weight, etc.)
+ * and provides functionality to read/write and manipulate entries.
+ */
 class Entry {
+    /**
+     * @var array $fields A list of fields the class uses.
+     */
     private static $fields = array(
         "url",         // URL of the link
         "displayName", // A name displayed instead of the raw URL
@@ -8,6 +14,15 @@ class Entry {
         "weight"       // Entries are sorted by ascending weight
     );
 
+    /**
+     * Constructs an entry from the given data array. Checks for the
+     * existence of field names (see @self::$fields) as keys in the
+     * given data array and if present, copies the values to the created
+     * instance.
+     *
+     * @param array $arr The data array
+     * @return Entry The created entry
+     */
     public static function createfromArray($arr) {
         $entry = new Entry();
         foreach (self::$fields as $fieldName) {
@@ -18,6 +33,16 @@ class Entry {
         return $entry;
     }
 
+    /**
+     * Reads entries from the given filename. The file is treated as CSV file
+     * and the first line is assumed to contain the headers. The entries are
+     * constructed from the data in the file under the assumption that the
+     * headers correspond to entry fields (see @self::$fields) in any order.
+     * 
+     * @param string The path and filename of the file to read
+     * @throws FileNotFoundException if the file wasn't found
+     * @return array A list of Entry instances
+     */
     public static function readFromCsvFile($filename) {
         $handle = fopen($filename, "r");
         if ($handle === FALSE) {
@@ -39,6 +64,14 @@ class Entry {
         return $entries;
     }
 
+    /**
+     * Writes the given list of entries to the given file. The file is created
+     * as CSV file with the fields of the entry (see @self::$fields) as headers.
+     *
+     * @param string $filename The path and filename where the file will be saved
+     * @param array $entries A list of entries
+     * @throws FileNotFoundException if the file could not be created/opened
+     */
     public static function writeToCsvFile($filename, $entries) {
         $handle = fopen($filename, "w");
         if ($handle === FALSE) {
@@ -60,6 +93,15 @@ class Entry {
         fclose($handle);
     }
 
+    /**
+     * Replaces or adds the given entry in the given list of entries. Looks
+     * for an entry with the exact same URL as the given one and replaces it
+     * completely if found.
+     *
+     * @param array $entries A list of entries
+     * @param Entry $newEntry The entry to add/replace
+     * @return array A list of entries including the new one
+     */
     public static function replaceOrAddEntryInList($entries, $newEntry) {
         $foundEntry = false;
         foreach ($entries as $index => $entry) {
@@ -76,6 +118,11 @@ class Entry {
         return $entries;
     }
 
+    /**
+     * Returns the entry as array with field=>value as key=>value pairs.
+     *
+     * @return array An array with the data of the entry
+     */
     public function toArray() {
         $arr = array();
         foreach (self::$fields as $fieldName) {
--- Section.php
+++ Section.php
@@ -1,11 +1,26 @@
 <?php
-
+/**
+ * Represents a section by which entries are grouped and provides functionality
+ * to read/write and manipulate sections.
+ */
 class Section {
+    /**
+     * @var array $fields A list of fields the class uses.
+     */
     private static $fields = array(
         "name",  // Name of the section
         "weight" // Sections are sorted by ascending weight
     );
 
+    /**
+     * Constructs a section from the given data array. Checks for the
+     * existence of field names (see @self::$fields) as keys in the
+     * given data array and if present, copies the values to the created
+     * instance.
+     *
+     * @param array $arr The data array
+     * @return Section The created section
+     */
     public static function createfromArray($arr) {
         $section = new Section();
         foreach (self::$fields as $fieldName) {
@@ -16,6 +31,16 @@ class Section {
         return $section;
     }
 
+    /**
+     * Reads sections from the given filename. The file is treated as CSV file
+     * and the first line is assumed to contain the headers. The sections are
+     * constructed from the data in the file under the assumption that the
+     * headers correspond to section fields (see @self::$fields) in any order.
+     * 
+     * @param string The path and filename of the file to read
+     * @throws FileNotFoundException if the file wasn't found
+     * @return array A list of Section instances
+     */
     public static function readFromCsvFile($filename) {
         $handle = fopen($filename, "r");
         if ($handle === FALSE) {
@@ -37,6 +62,13 @@ class Section {
         return $sections;
     }
 
+    /**
+     * Adds the given section to the given list of sections, if it does not
+     * already exist, identified by exact name.
+     *
+     * @param array $sections A list of sections
+     * @param Section $newSection A section to insert/update
+     */
     public static function addIfNotExists($sections, $newSection) {
         $foundSection = false;
         foreach ($sections as $section) {
@@ -52,6 +84,14 @@ class Section {
         return $sections;
     }
 
+    /**
+     * Writes the given list of sections to the given file. The file is created
+     * as CSV file with the fields of the section (see @self::$fields) as headers.
+     *
+     * @param string $filename The path and filename where the file will be saved
+     * @param array $sections A list of sections
+     * @throws FileNotFoundException if the file could not be created/opened
+     */
     public static function writeToCsvFile($filename, $sections) {
         $handle = fopen($filename, "w");
         if ($handle === FALSE) {
@@ -71,6 +111,11 @@ class Section {
         fclose($handle);
     }
 
+    /**
+     * Returns the section as array with field=>value as key=>value pairs.
+     *
+     * @return array An array with the data of the section
+     */
     public function toArray() {
         $arr = array();
         foreach (self::$fields as $fieldName) {
