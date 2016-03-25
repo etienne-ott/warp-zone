8cf69d341953696f4bbf9538127bfa6cb68380f0
Etienne Ott <eti.ott@gmail.com>
2016-03-25 17:16:29 +0100

    Prepare entities Entry and Section for database de-/serialization.

--- library/WarpZone/Entity/Entry.php
+++ library/WarpZone/Entity/Entry.php
@@ -10,6 +10,138 @@ class Entry
 {
     const CSV_SEP_CHAR = ",";
 
+    protected $entryId;
+
+    protected $url;
+
+    protected $displayName;
+
+    protected $section;
+
+    protected $priority;
+
+    public function __construct($id)
+    {
+        $this->entryId = $id;
+    }
+
+    public function getEntryId()
+    {
+        return $this->entryId;
+    }
+
+    public function getUrl()
+    {
+        return $this->url;
+    }
+
+    public function setUrl($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
+    public function getDisplayName()
+    {
+        return $this->displayName;
+    }
+
+    public function setDisplayName($name)
+    {
+        $this->displayName = $name;
+        return $this;
+    }
+
+    public function getSection()
+    {
+        return $this->section;
+    }
+
+    public function setSection(\WarpZone\Entity\Section $section)
+    {
+        $this->section = $section;
+        return $this;
+    }
+
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    public function setPriority($prio)
+    {
+        $this->priority = $prio;
+        return $this;
+    }
+
+    public static function findOneByEntryId($id)
+    {
+        $db  = \Slim\Slim::getInstance()->config('db');
+        $sql = "SELECT *
+                FROM entry
+                WHERE entry_id = :id";
+
+        $result = $db->executeQuery($sql, array('id' => $id));
+        $row    = $result->fetch();
+
+        if (!isset($row['entry_id'])) {
+            return null;
+        }
+
+        if (isset($row['section_id']) && !empty($row['section_id'])) {
+            $section = \WarpZone\Entity\Section::findOneByEntryId($row['section_id']);
+        } else {
+            $section = null;
+        }
+
+        $entry = new self((int)$row['entry_id']);
+        $entry->setUrl($row['url'])
+            ->setDisplayName($row['display_name'])
+            ->setSection($section)
+            ->setPriority((int)$row['priority']);
+
+        return $entry;
+    }
+
+    public static function findAll()
+    {
+        $db  = \Slim\Slim::getInstance()->config('db');
+        $sql = "SELECT *
+                FROM entry";
+
+        $result = $db->executeQuery($sql);
+        $rows   = $result->fetchAll();
+
+        if (empty($rows)) {
+            return null;
+        }
+
+        $sections = array();
+        $entries  = array();
+
+        foreach ($rows as $row) {
+            if (isset($row['section_id']) && !empty($row['section_id'])) {
+                if (isset($sections[$row['section_id']])) {
+                    $section = $sections[$row['section_id']];
+                } else {
+                    $section = \WarpZone\Entity\Section::findOneBySectionId($row['section_id']);
+                    $sections[$row['section_id']] = $section;
+                }
+            } else {
+                $section = null;
+            }
+
+            $entry = new self((int)$row['entry_id']);
+            $entry->setUrl($row['url'])
+                ->setDisplayName($row['display_name'])
+                ->setSection($section)
+                ->setPriority((int)$row['priority']);
+            $entries[] = $entry;
+        }
+
+        return $entries;
+    }
+
     /**
      * @var array $fields A list of fields the class uses.
      */
--- library/WarpZone/Entity/Section.php
+++ library/WarpZone/Entity/Section.php
@@ -10,6 +10,90 @@ class Section
 {
     const CSV_SEP_CHAR = ",";
 
+    protected $sectionId;
+
+    protected $name;
+
+    protected $priority;
+
+    public function __construct($id)
+    {
+        $this->sectionId = $id;
+    }
+
+    public function getSectionId()
+    {
+        return $this->sectionId;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+
+    public function setPriority($prio)
+    {
+        $this->priority = $prio;
+        return $this;
+    }
+
+    public static function findOneBySectionId($id)
+    {
+        $db  = \Slim\Slim::getInstance()->config('db');
+        $sql = "SELECT *
+                FROM section
+                WHERE section_id = :id";
+
+        $result = $db->executeQuery($sql, array('id' => $id));
+        $row    = $result->fetch();
+
+        if (!isset($row['section_id'])) {
+            return null;
+        }
+
+        $section = new self((int)$row['section_id']);
+        $section->setName($row['name'])
+            ->setPriority((int)$row['priority']);
+
+        return $section;
+    }
+
+    public static function findAll()
+    {
+        $db  = \Slim\Slim::getInstance()->config('db');
+        $sql = "SELECT *
+                FROM section";
+
+        $result = $db->executeQuery($sql);
+        $rows   = $result->fetchAll();
+
+        if (empty($rows)) {
+            return null;
+        }
+
+        $sections = array();
+
+        foreach ($rows as $row) {
+            $section = new self((int)$row['section_id']);
+            $section->setName($row['name'])
+                ->setPriority((int)$row['priority']);
+            $sections[] = $section;
+        }
+
+        return $sections;
+    }
+
     /**
      * @var array $fields A list of fields the class uses.
      */
