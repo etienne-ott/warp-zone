d859b7f66f8a2026eef0719f331d8c766015b52f
Etienne Ott <eti.ott@gmail.com>
2016-03-25 15:22:45 +0100

    Implement action changeTheme to update the selected theme.

--- library/WarpZone/Controller/Setting.php
+++ library/WarpZone/Controller/Setting.php
@@ -13,6 +13,17 @@ class Setting extends \WarpZone\Controller\AbstractController
      */
     public function changeThemeAction($args)
     {
+        $name = isset($_POST['theme']) ? $_POST['theme'] : '';
+        if (file_exists(APPLICATION_PATH . '/styles/' . $name . '.less')) {
+            $config = \Slim\Slim::getInstance()->config('settings');
+            $config->Theme->active_theme = $name;
+            $config->writeToIniFile(APPLICATION_PATH . '/config.ini');
+            \Slim\Slim::getInstance()->config('settings', $config);
+        } else {
+            // Eventually we will print an error message via the flash messenger
+            // but there is no such thing yet
+        }
 
+        $this->redirect('/rebuild');
     }
 }
