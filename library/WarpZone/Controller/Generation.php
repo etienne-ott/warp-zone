4c55c639ba0815a858111a9541036ac7f3cb5f13
Etienne Ott <eti.ott@gmail.com>
2016-03-26 14:38:07 +0100

    Handle form data for new entries.

--- library/WarpZone/Controller/Generation.php
+++ library/WarpZone/Controller/Generation.php
@@ -1,6 +1,9 @@
 <?php
 namespace WarpZone\Controller;
 
+use WarpZone\Entity\Section,
+    WarpZone\Entity\Entry,
+    WarpZone\ElementFormat;
 /**
  * Handles request for the frontpage.
  */
@@ -13,6 +16,10 @@ class Generation extends \WarpZone\Controller\AbstractController
      */
     public function rebuildAction($args)
     {
+        if (!empty($_POST)) {
+            $this->handleEntryFormData($_POST);
+        }
+
         $generator = new \WarpZone\Generator();
         $settings  = \Slim\Slim::getInstance()->config('settings');
         $errors    = $generator->rebuildMain($settings);
@@ -23,5 +30,33 @@ class Generation extends \WarpZone\Controller\AbstractController
         }
 
         $this->redirect('/');
+    }
+
+    protected function handleEntryFormData($data)
+    {
+        if (
+            isset($data['url'])
+            && isset($data['displayName'])
+            && (
+                isset($data['section'])
+                || isset($data['newSection'])
+            )
+        ) {
+            $url     = $data['url'];
+            $name    = $data['displayName'];
+            $section = isset($data['section']) ? $data['section'] : ElementFormat::DEFAULT_SECTION;
+            $newSec  = isset($data['newSection']) ? $data['newSection'] : null;
+
+            if (!empty($newSec)) {
+                $section = $newSec;
+            }
+
+            $found = Section::findOneByName($section);
+            if (!$found instanceof Section) {
+                $found = Section::create($section, 0);
+            }
+
+            $entry = Entry::create($url, $name, $found, 0);
+        }
     }
 }
