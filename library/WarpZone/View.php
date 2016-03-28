1f0a9bc8cc7812417576fe76cc5534a86951ef0e
Etienne Ott <eti.ott@gmail.com>
2016-03-28 13:51:42 +0200

    Add methods and fields for form data and errors to class View.

--- library/WarpZone/View.php
+++ library/WarpZone/View.php
@@ -1,12 +1,18 @@
 <?php
 namespace WarpZone;
 
+use WarpZone\ElementFormat;
+
 class View extends \Slim\View
 {
     /**
      * @var string The base URL of our application.
      */
     protected $_baseUrl = null;
+
+    protected $formData = array();
+
+    protected $errors = array();
 
     /**
      * @see \Slim\View::render()
@@ -40,6 +46,28 @@ class View extends \Slim\View
     public function setBaseUrl($url)
     {
         $this->_baseUrl = $url;
+        return $this;
+    }
+
+    public function getFormData()
+    {
+        return $this->formData;
+    }
+
+    public function setFormData($data)
+    {
+        $this->formData = $data;
+        return $this;
+    }
+
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+
+    public function setErrors($errors)
+    {
+        $this->errors = $errors;
         return $this;
     }
 
@@ -91,5 +119,38 @@ class View extends \Slim\View
             this.system = {}
             this.system.baseUrl = '%s'
         }", $this->url('/'));
+    }
+
+    /**
+     * Tries to extract the form data for the field with the given
+     * name from the view. If not found, returns an empty string.
+     *
+     * @param string $name The name of the form field
+     * @return string The form data for the given name or an empty string
+     */
+    public function formField($name)
+    {
+        return isset($this->formData[$name]) ? $this->formData[$name] : '';
+    }
+
+    /**
+     * Tries to extract the form error for the field with the given name
+     * from the view. If not found, returns an empty string.
+     * If the formatted flag was set, automatically formats the errors. If
+     * no error was found, returns an empty string instead of a formatted
+     * empty error. This is useful so the method can always be called,
+     * regardless of whether there was an error or not.
+     *
+     * @param string The name of the form field
+     * @param boolean $doFormat (Optional) If true, formats the error.
+     *    Defaults to true.
+     * @return string A (possibly empty) string with the error. May be
+     *    formatted.
+     */
+    public function formError($name, $doFormat = true)
+    {
+        return isset($this->errors[$name])
+            ? ElementFormat::formatError($this->errors[$name])
+            : '';
     }
 }
